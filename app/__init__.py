b9fadbc8f0dbc5b39e37a25420bb96b58143f0a6
Tamirlan Omarov <omarovt96@gmail.com>
2018-10-25 22:33:22 +0300

    Chapter 7 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+import os
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -13,4 +17,34 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    # sending email for ERROR level logs
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_SERVER']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_SERVER'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog failure',
+            credentials=auth, secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+
+    # writing logs to file
+    if not os.path.exists('log'):
+        os.mkdir('log')
+
+    file_handler = RotatingFileHandler('log/microblog.log', maxBytes=10240, backupCount=10)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
--- app/config.py
+++ app/config.py
@@ -7,3 +7,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdkjas3njadnj23nrn2mlsadasda'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # mail server configuration
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 25
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['omarovt96@gmail.com']
