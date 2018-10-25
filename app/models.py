795b9badda21bf5febca58861e31ab3ee18667e0
Tamirlan Omarov <omarovt96@gmail.com>
2018-10-25 21:28:56 +0300

    Chapter 6 completed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import datetime
 
 from flask_login import UserMixin
@@ -6,12 +7,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
 
+GRAVATAR_URL = 'https://www.gravatar.com/avatar/{emailhex}?d=identicon&s={size}'
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,6 +27,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size=80):
+        emailhex = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return GRAVATAR_URL.format(emailhex=emailhex, size=size)
 
 
 class Post(db.Model):
